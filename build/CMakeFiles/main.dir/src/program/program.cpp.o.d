3858d2aff66303bbe328871814d8d7d32052542b
ZDev22 <adamorrtexas@gmail.com>
2025-06-22 21:57:28 -0500

    ok wait

--- build/CMakeFiles/main.dir/src/program/program.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/main.dir/src/program/program.cpp.o: \
- /mnt/chromeos/MyFiles/Downloads/vulkanProjects/src/program/program.cpp \
- /usr/include/stdc-predef.h \
- /mnt/chromeos/MyFiles/Downloads/vulkanProjects/src/program/program.hpp
